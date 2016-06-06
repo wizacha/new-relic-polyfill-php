c17dde1f1739e2fb68fe06fafddc63e57b125b6d
Matthieu Napoli <matthieu@mnapoli.fr>
2016-06-06 18:32:52 +0200

    First commit

---
+++ src/NewRelic.php
@@ -0,0 +1,77 @@
+<?php
+
+if (!extension_loaded('newrelic')) {
+
+    function newrelic_add_custom_parameter()
+    {
+    }
+
+    function newrelic_add_custom_tracer()
+    {
+    }
+
+    function newrelic_background_job()
+    {
+    }
+
+    function newrelic_capture_params()
+    {
+    }
+
+    function newrelic_custom_metric()
+    {
+    }
+
+    function newrelic_disable_autorum()
+    {
+    }
+
+    function newrelic_end_of_transaction()
+    {
+    }
+
+    function newrelic_end_transaction()
+    {
+    }
+
+    function newrelic_get_browser_timing_footer()
+    {
+    }
+
+    function newrelic_get_browser_timing_header()
+    {
+    }
+
+    function newrelic_ignore_apdex()
+    {
+    }
+
+    function newrelic_ignore_transaction()
+    {
+    }
+
+    function newrelic_name_transaction()
+    {
+    }
+
+    function newrelic_notice_error()
+    {
+    }
+
+    function newrelic_record_custom_event()
+    {
+    }
+
+    function newrelic_set_appname()
+    {
+    }
+
+    function newrelic_set_user_attributes()
+    {
+    }
+
+    function newrelic_start_transaction()
+    {
+    }
+
+}
